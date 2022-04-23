2db689b793051f8f67890f78e055f9f685ee313a
Sangdon Park <sangdonp@seas.upenn.edu>
2022-04-23 17:21:58 -0400

    domainnet/twonormals scripts are updated and tested.

--- scripts/run_main_cls_twonormals.sh
+++ scripts/run_main_cls_twonormals.sh
@@ -1,6 +1,8 @@
 GPU_ID=2
 EPS=0.01
 DELTA=1e-5
+DELTAIW=0.5e-5
+DELTACAL=0.5e-5
 
 M=50000
 
@@ -10,6 +12,7 @@ TAR=ThinNormal
 EXPNAME=twonormals_src_${SRC}_tar_${TAR}_linear
 
 MDLPATH=snapshots_models/TwoNormals/twonormals_src_${SRC}_tar_${TAR}_linear/model_params_best
+SDMDLPATH=snapshots_models/TwoNormals/twonormals_src_${SRC}_tar_${TAR}_linear/model_params_srcdisc_best
 
 for i in {1..100}
 do
@@ -33,7 +36,8 @@ do
 			--data.seed None \
 			--model_predset.alpha $EPS \
 			--data.n_val_src $M \
-			--model.path_pretrained $MDLPATH 
+			--model.path_pretrained $MDLPATH \
+			--model_sd.path_pretrained $SDMDLPATH 
     # PS-C
     CUDA_VISIBLE_DEVICES=$GPU_ID python3 main_cls_twonormals.py \
 			--exp_name exp_${EXPNAME}_maxiw_m_${M}_eps_${EPS}_delta_${DELTA}_expid_$i \
@@ -44,7 +48,8 @@ do
 			--model_predset.eps $EPS \
 			--model_predset.delta $DELTA \
 			--data.n_val_src $M \
-			--model.path_pretrained $MDLPATH 
+			--model.path_pretrained $MDLPATH \
+			--model_sd.path_pretrained $SDMDLPATH 
     # PS-R
     CUDA_VISIBLE_DEVICES=$GPU_ID python3 main_cls_twonormals.py \
 			--exp_name exp_${EXPNAME}_rejection_m_${M}_eps_${EPS}_delta_${DELTA}_expid_$i \
@@ -55,6 +60,20 @@ do
 			--model_predset.eps $EPS \
 			--model_predset.delta $DELTA \
 			--data.n_val_src $M \
-			--model.path_pretrained $MDLPATH 
+			--model.path_pretrained $MDLPATH \
+			--model_sd.path_pretrained $SDMDLPATH
+    # PS-W
+    CUDA_VISIBLE_DEVICES=$GPU_ID python3 main_cls_twonormals.py \
+			--exp_name exp_${EXPNAME}_worst_rejection_m_${M}_eps_${EPS}_delta_${DELTA}_expid_$i \
+			--data.src $SRC \
+			--data.tar $TAR \
+			--train_predset.method pac_predset_worst_rejection \
+			--data.seed None \
+			--model_predset.eps $EPS \
+			--model_predset.delta $DELTACAL \
+			--model_iwcal.delta $DELTAIW \
+			--data.n_val_src $M \
+			--model.path_pretrained $MDLPATH \
+			--model_sd.path_pretrained $SDMDLPATH 
 done 
 
